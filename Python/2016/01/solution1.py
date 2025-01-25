729cbcc943427b7b6a0e34a5dcd59861edd498a0
okraus <okraus42+github@pm.me>
2025-01-25 17:21:54 +0100

    2016 easy start

--- Python/2016/01/solution1.py
+++ Python/2016/01/solution1.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
 
-import math
-import sys
+from enum import Enum
+
+class Direction(Enum):
+	NORTH = 0
+	EAST = 1
+	SOUTH = 2
+	WEST = 3
+
+	@staticmethod
+	def turn_clockwise(direction):
+		# Increase the direction by 1 (modulo 4)
+		return Direction((direction.value + 1) % 4)
+
+	@staticmethod
+	def turn_anticlockwise(direction):
+		# Decrease the direction by 1 (modulo 4)
+		return Direction((direction.value - 1) % 4)
+
+
 FILE_NAME = 'input.txt'
 
 # Open the file in read mode
@@ -9,7 +26,28 @@ with open(FILE_NAME, 'r') as file:
 	# Read the entire content of the file into a string
 	content = file.read()
 
+parts = content.split(', ')
+directions = []
+for part in parts:
+	letter = part[0]
+	value = int(part[1:])
+	directions.append((letter, value))
 result = 0
-# Now, 'content' contains the file's content as a string
-
+current_direction = Direction.NORTH
+y = 0
+x = 0
+for direction in directions:
+	if direction[0] == 'R':
+		current_direction = Direction.turn_clockwise(current_direction)
+	else:
+		current_direction = Direction.turn_anticlockwise(current_direction)
+	if current_direction == Direction.NORTH:
+		y += direction[1]
+	elif current_direction == Direction.EAST:
+		x += direction[1]
+	elif current_direction == Direction.SOUTH:
+		y -= direction[1]
+	elif current_direction == Direction.WEST:
+		x -= direction[1]
+result = abs(x) + abs(y)
 print(result)
--- Python/2016/01/solution2.py
+++ Python/2016/01/solution2.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
 
-import math
-import sys
+from enum import Enum
+
+class Direction(Enum):
+	NORTH = 0
+	EAST = 1
+	SOUTH = 2
+	WEST = 3
+
+	@staticmethod
+	def turn_clockwise(direction):
+		# Increase the direction by 1 (modulo 4)
+		return Direction((direction.value + 1) % 4)
+
+	@staticmethod
+	def turn_anticlockwise(direction):
+		# Decrease the direction by 1 (modulo 4)
+		return Direction((direction.value - 1) % 4)
+
+
 FILE_NAME = 'input.txt'
 
 # Open the file in read mode
@@ -9,15 +26,39 @@ with open(FILE_NAME, 'r') as file:
 	# Read the entire content of the file into a string
 	content = file.read()
 
+parts = content.split(', ')
+directions = []
+for part in parts:
+	letter = part[0]
+	value = int(part[1:])
+	directions.append((letter, value))
 result = 0
-floor = 0
-# Now, 'content' contains the file's content as a string
-for char in content:
-	result += 1
-	if char == '(':
-		floor += 1
-	if char == ')':
-		floor -= 1
-	if (floor == -1):
-		print(result)
+current_direction = Direction.NORTH
+y = 0
+x = 0
+visited = set()
+visited.add((x, y))
+found = False
+for direction in directions:
+	if direction[0] == 'R':
+		current_direction = Direction.turn_clockwise(current_direction)
+	else:
+		current_direction = Direction.turn_anticlockwise(current_direction)
+	for _ in range(direction[1]):
+		if current_direction == Direction.NORTH:
+			y += 1
+		elif current_direction == Direction.EAST:
+			x += 1
+		elif current_direction == Direction.SOUTH:
+			y -= 1
+		elif current_direction == Direction.WEST:
+			x -= 1
+		if (x, y) in visited:
+			found = True
+			break
+		else:
+			visited.add((x, y))
+	if (found):
 		break
+result = abs(x) + abs(y)
+print(result)
