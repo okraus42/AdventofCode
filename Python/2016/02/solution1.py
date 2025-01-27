54e769dc64c2f1f0b8382cf3356291005244a71a
okraus <okraus42+github@pm.me>
2025-01-27 19:41:10 +0100

    solution

--- Python/2016/02/solution1.py
+++ Python/2016/02/solution1.py
@@ -8,7 +8,23 @@ FILE_NAME = 'input.txt'
 with open(FILE_NAME, 'r') as file:
 	# Read the entire content of the file into a string
 	lines = file.readlines()
-
+numpad = [	[1, 2, 3],
+			[4, 5, 6],
+			[7, 8, 9]]
+y = 1
+x = 1
 result = 0
+for line in lines:
+	for char in line:
+		if char == 'D' and y < 2:
+			y += 1
+		elif char == 'U' and y > 0:
+			y -= 1
+		elif char == 'R' and x < 2:
+			x += 1
+		elif char == 'L' and x > 0:
+			x -= 1
+	result *= 10
+	result += numpad[y][x]
 # need to update
 print(result)
--- Python/2016/02/solution2.py
+++ Python/2016/02/solution2.py
@@ -8,12 +8,28 @@ FILE_NAME = 'input.txt'
 with open(FILE_NAME, 'r') as file:
 	# Read the entire content of the file into a string
 	lines = file.readlines()
-
-result = 0
-# Now, 'content' contains the file's content as a string
+numpad = 	[
+				[' ', ' ', ' ', ' ', ' ', ' ', ' '],
+				[' ', ' ', ' ', '1', ' ', ' ', ' '],
+				[' ', ' ', '2', '3', '4', ' ', ' '],
+				[' ', '5', '6', '7', '8', '9', ' '],
+				[' ', ' ', 'A', 'B', 'C', ' ', ' '],
+				[' ', ' ', ' ', 'D', ' ', ' ', ' '],
+				[' ', ' ', ' ', ' ', ' ', ' ', ' ']
+			]
+y = 3
+x = 3
+result = ''
 for line in lines:
-	numbers = line.split('x')
-	a, b, c = map(int, numbers)
-	a, b, c = sorted([a, b, c])
-	result += 2*a + 2*b + a*b*c
+	for char in line:
+		if char == 'D' and numpad[y + 1][x] != ' ':
+			y += 1
+		elif char == 'U' and numpad[y - 1][x] != ' ':
+			y -= 1
+		elif char == 'R' and numpad[y][x + 1] != ' ':
+			x += 1
+		elif char == 'L' and numpad[y][x - 1] != ' ':
+			x -= 1
+	result += numpad[y][x]
+# need to update
 print(result)
