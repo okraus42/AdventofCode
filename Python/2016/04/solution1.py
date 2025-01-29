25d4f8670b48a62db9f55880cba24f3484c457d1
okraus <okraus42+github@pm.me>
2025-01-29 20:18:39 +0100

    day 4 :)

--- Python/2016/04/solution1.py
+++ Python/2016/04/solution1.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
 
-import math
-import sys
-import hashlib
+from collections import Counter
 
 FILE_NAME = 'input.txt'
 
 # Open the file in read mode
 with open(FILE_NAME, 'r') as file:
 	# Read the entire content of the file into a string
-	content = file.read()
+	lines = file.readlines()
 
 result = 0
-
-while True:
-	md5_hash = hashlib.md5((content + str(result)).encode()).hexdigest()
-	if md5_hash[:5] == '00000':
-		break
-	result += 1
+for line in lines:
+	main_part, room_name = line.split('[')
+	room_name = room_name.strip('\n')
+	# Remove the closing bracket from room name
+	room_name = room_name.strip(']')
+	# Split the main part into words and room id
+	parts = main_part.rsplit('-', maxsplit=1)
+	# The words are all parts except the last one
+	words = parts[0].split('-')
+	# The room ID is the last part
+	room_id = int(parts[1])
+	letters = ''.join(words)
+	freq = Counter(letters)
+	name = ''.join(sorted(set(letters), key=lambda x: (-freq[x], x)))
+	if name[:len(room_name)] == room_name:
+		result += room_id
 print(result)
--- Python/2016/04/solution2.py
+++ Python/2016/04/solution2.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python3
 
-import math
-import sys
-import hashlib
+from collections import Counter
 
 FILE_NAME = 'input.txt'
 
 # Open the file in read mode
 with open(FILE_NAME, 'r') as file:
 	# Read the entire content of the file into a string
-	content = file.read()
+	lines = file.readlines()
 
 result = 0
-
-while True:
-	md5_hash = hashlib.md5((content + str(result)).encode()).hexdigest()
-	if md5_hash[:6] == '000000':
-		break
-	result += 1
+for line in lines:
+	main_part, room_name = line.split('[')
+	room_name = room_name.strip('\n')
+	# Remove the closing bracket from room name
+	room_name = room_name.strip(']')
+	# Split the main part into words and room id
+	parts = main_part.rsplit('-', maxsplit=1)
+	name = parts[0].replace("-", " ")
+	# The room ID is the last part
+	room_id = int(parts[1])
+	secret_name = []
+	for char in name:
+		if char == ' ':
+			secret_name.append(' ')
+			continue
+		num = ord(char) - ord('a')
+		num += room_id
+		num %= 26
+		new_char = chr(num + ord('a'))
+		secret_name.append(new_char)
+	secret_name = ''.join(secret_name)
+	if "north" in secret_name:
+		result = room_id
 print(result)
