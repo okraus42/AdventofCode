1c8167b8591e484dfd38e37281fb0788065d6f58
okraus <okraus42+github@pm.me>
2025-01-31 17:08:14 +0100

    day6

--- Python/2016/06/solution1.py
+++ Python/2016/06/solution1.py
@@ -10,34 +10,25 @@ with open(FILE_NAME, 'r') as file:
 	lines = file.readlines()
 
 lights = [[False] * 1000 for _ in range(1000)]
-result = 0
+result = ''
 # Now, 'content' contains the file's content as a string
+counts = [
+	{},
+	{},
+	{},
+	{},
+	{},
+	{},
+	{},
+	{}
+]
 for line in lines:
-	if "turn on" in line:
-		action = "turn on"
-	elif "turn off" in line:
-		action = "turn off"
-	elif "toggle" in line:
-		action = "toggle"
-	# Remove the action from the line
-	parts = line.replace(action, "").strip().split(" through ")
-	# Extract coordinates
-	x1, y1 = map(int, parts[0].split(","))
-	x2, y2 = map(int, parts[1].split(","))
-	if (action == "toggle"):
-		for y in range(y1, y2 + 1):
-			for x in range(x1, x2 + 1):
-				lights[y][x] = not lights[y][x]
-	if (action == "turn on"):
-		for y in range(y1, y2 + 1):
-			for x in range(x1, x2 + 1):
-				lights[y][x] = True
-	if (action == "turn off"):
-		for y in range(y1, y2 + 1):
-			for x in range(x1, x2 + 1):
-				lights[y][x] = False
-for y in range(0, 1000):
-	for x in range(0, 1000):
-		if lights[y][x]:
-			result += 1
+	line = line.strip()
+	for index, char in enumerate(line):
+		if char in counts[index]:
+			counts[index][char] += 1
+		else:
+			counts[index][char] = 1
+for c in counts:
+	result += max(c, key=c.get)
 print(result)
--- Python/2016/06/solution2.py
+++ Python/2016/06/solution2.py
@@ -9,35 +9,26 @@ with open(FILE_NAME, 'r') as file:
 	# Read the entire content of the file into a string
 	lines = file.readlines()
 
-lights = [[0] * 1000 for _ in range(1000)]
-result = 0
+lights = [[False] * 1000 for _ in range(1000)]
+result = ''
 # Now, 'content' contains the file's content as a string
+counts = [
+	{},
+	{},
+	{},
+	{},
+	{},
+	{},
+	{},
+	{}
+]
 for line in lines:
-	if "turn on" in line:
-		action = "turn on"
-	elif "turn off" in line:
-		action = "turn off"
-	elif "toggle" in line:
-		action = "toggle"
-	# Remove the action from the line
-	parts = line.replace(action, "").strip().split(" through ")
-	# Extract coordinates
-	x1, y1 = map(int, parts[0].split(","))
-	x2, y2 = map(int, parts[1].split(","))
-	if (action == "toggle"):
-		for y in range(y1, y2 + 1):
-			for x in range(x1, x2 + 1):
-				lights[y][x] += 2
-	if (action == "turn on"):
-		for y in range(y1, y2 + 1):
-			for x in range(x1, x2 + 1):
-				lights[y][x] += 1
-	if (action == "turn off"):
-		for y in range(y1, y2 + 1):
-			for x in range(x1, x2 + 1):
-				if lights[y][x] != 0:
-					lights[y][x] -= 1
-for y in range(0, 1000):
-	for x in range(0, 1000):
-		result += lights[y][x]
+	line = line.strip()
+	for index, char in enumerate(line):
+		if char in counts[index]:
+			counts[index][char] += 1
+		else:
+			counts[index][char] = 1
+for c in counts:
+	result += min(c, key=c.get)
 print(result)
