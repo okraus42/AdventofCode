e9d786d1fe418b80ac03bbd0161ea9bcf8ddb8cc
okraus <okraus42+github@pm.me>
2025-02-11 16:16:40 +0100

    prep

---
+++ Python/2015/25/solution1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+FILE_NAME = 'input.txt'
+
+# Read the file
+with open(FILE_NAME, 'r') as file:
+	lines = [line.strip() for line in file.readlines()]
+
+
+# Parse instructions from the file
+for line in lines:
+	pass
+
+result = 0
+print(result)
