e17ff21b99dcfa9b617f047914fc289167893222
okraus <okraus42+github@pm.me>
2025-12-02 06:52:44 +0100

    Ugly solution but it works

--- C++/2025/02/solver.cpp
+++ C++/2025/02/solver.cpp
@@ -2,17 +2,307 @@
 #include <fstream>
 #include <string>
 #include <cctype>
-#include <set>
+#include <vector>
 #include <sstream>
 
-void convertStringToIntegers(const std::string& str, int& a) {
-	std::stringstream ss(str);
-	ss >> a;
+void convertStringToIntegers(const std::string& str, std::vector<long long>& numbers) {
+	std::string cleaned = str;
+	for (char& c : cleaned)
+		if (!isdigit(c)) c = ' ';
+
+	std::stringstream ss(cleaned);
+	long long num;
+	while (ss >> num) {
+		numbers.push_back(num);
+	}
+}
+
+void convertIntegerToString(const long long num, std::string& str) {
+	std::stringstream ss;
+	ss << num;
+	ss >> str;
+}
+
+long long	find_invalids(long long a, long long b) {
+	long long invalids = 0;
+	int invalid;
+	std::string str;
+	// convertIntegerToString(a, str);
+	// std::cout << str << std::endl;
+	for (long long l = a; l <= b; ++l) {
+		str.clear();
+		convertIntegerToString(l, str);
+		int n = str.size();
+		if (n % 2 == 0)
+		{
+			invalid = 1;
+			for (int i = 0; i < n / 2; i++) {
+				// std::cout << str[i] << " " << str[n / 2 + i] << std::endl;
+				if (str[i] != str[n / 2 + i])
+					invalid = 0;
+			}
+			if (invalid)
+				invalids += l;
+
+		}
+	}
+	return invalids;
+}
+
+long long	find_invalids2(long long a, long long b) {
+	long long invalids = 0;
+	int invalid;
+	std::string str;
+	// convertIntegerToString(a, str);
+	// std::cout << str << std::endl;
+	for (long long l = a; l <= b; ++l) {
+		str.clear();
+		convertIntegerToString(l, str);
+		int n = str.size();
+		if (n % 2 == 0)
+		{
+			invalid = 1;
+			for (int i = 0; i < n / 2; i++) {
+				// std::cout << str[i] << " " << str[n / 2 + i] << std::endl;
+				if (str[i] != str[n / 2 + i])
+					invalid = 0;
+			}
+			if (invalid) {
+				invalids += l;
+				std::cout << l << std::endl;
+				continue; 
+			}
+		}
+		if (n % 3 == 0)
+		{
+			invalid = 1;
+			for (int i = 0; i < n / 3; i++) {
+				if (!(str[i] == str[n / 3 + i]
+					&& str[i] == str[2 * n / 3 + i]
+					))
+					invalid = 0;
+			}
+			if (invalid) {
+				invalids += l;
+				std::cout << l << std::endl;
+				continue; 
+			}
+		}
+		if (n % 4 == 0)
+		{
+			invalid = 1;
+			for (int i = 0; i < n / 4; i++) {
+				if (!((str[i] == str[n / 4 + i]
+					&& str[i] == str[2 * n / 4 + i]
+					&& str[i] == str[3 * n / 4 + i]))
+				)
+					invalid = 0;
+			}
+			if (invalid) {
+				invalids += l;
+				std::cout << l << std::endl;
+				continue; 
+			}
+		}
+		if (n % 5 == 0)
+		{
+			invalid = 1;
+			for (int i = 0; i < n / 5; i++) {
+				if (!(str[i] == str[n / 5 + i]
+					&& str[i] == str[2 * n / 5 + i]
+					&& str[i] == str[3 * n / 5 + i]
+					&& str[i] == str[4 * n / 5 + i]
+				))
+					invalid = 0;
+			}
+			if (invalid) {
+				invalids += l;
+				std::cout << l << std::endl;
+				continue; 
+			}
+		}
+		if (n % 6 == 0)
+		{
+			invalid = 1;
+			for (int i = 0; i < n / 6; i++) {
+				if (!(str[i] == str[n / 6 + i]
+					&& str[i] == str[2 * n / 6 + i]
+					&& str[i] == str[3 * n / 6 + i]
+					&& str[i] == str[4 * n / 6 + i]
+					&& str[i] == str[5 * n / 6 + i]
+				))
+					invalid = 0;
+			}
+			if (invalid) {
+				invalids += l;
+				std::cout << l << std::endl;
+				continue; 
+			}
+		}
+		if (n % 7 == 0)
+		{
+			invalid = 1;
+			for (int i = 0; i < n / 7; i++) {
+				if (!(str[i] == str[n / 7 + i]
+					&& str[i] == str[2 * n / 7 + i]
+					&& str[i] == str[3 * n / 7 + i]
+					&& str[i] == str[4 * n / 7 + i]
+					&& str[i] == str[5 * n / 7 + i]
+					&& str[i] == str[6 * n / 7 + i]
+				))
+					invalid = 0;
+			}
+			if (invalid) {
+				invalids += l;
+				std::cout << l << std::endl;
+				continue; 
+			}
+		}
+		if (n % 8 == 0)
+		{
+			invalid = 1;
+			for (int i = 0; i < n / 8; i++) {
+				if (!(str[i] == str[n / 8 + i]
+					&& str[i] == str[2 * n / 8 + i]
+					&& str[i] == str[3 * n / 8 + i]
+					&& str[i] == str[4 * n / 8 + i]
+					&& str[i] == str[5 * n / 8 + i]
+					&& str[i] == str[6 * n / 8 + i]
+					&& str[i] == str[7 * n / 8 + i]
+				))
+					invalid = 0;
+			}
+			if (invalid) {
+				invalids += l;
+				std::cout << l << std::endl;
+				continue; 
+			}
+		}
+		if (n % 9 == 0)
+		{
+			invalid = 1;
+			for (int i = 0; i < n / 9; i++) {
+				if (!(str[i] == str[n / 9 + i]
+					&& str[i] == str[2 * n / 9 + i]
+					&& str[i] == str[3 * n / 9 + i]
+					&& str[i] == str[4 * n / 9 + i]
+					&& str[i] == str[5 * n / 9 + i]
+					&& str[i] == str[6 * n / 9 + i]
+					&& str[i] == str[7 * n / 9 + i]
+					&& str[i] == str[8 * n / 9 + i]
+				))
+					invalid = 0;
+			}
+			if (invalid) {
+				invalids += l;
+				std::cout << l << std::endl;
+				continue; 
+			}
+		}
+		if (n % 10 == 0)
+		{
+			invalid = 1;
+			for (int i = 0; i < n / 10; i++) {
+				if (!(str[i] == str[n / 10 + i]
+					&& str[i] == str[2 * n / 10 + i]
+					&& str[i] == str[3 * n / 10 + i]
+					&& str[i] == str[4 * n / 10 + i]
+					&& str[i] == str[5 * n / 10 + i]
+					&& str[i] == str[6 * n / 10 + i]
+					&& str[i] == str[7 * n / 10 + i]
+					&& str[i] == str[8 * n / 10 + i]
+					&& str[i] == str[9 * n / 10 + i]
+				))
+					invalid = 0;
+			}
+			if (invalid) {
+				invalids += l;
+				std::cout << l << std::endl;
+				continue; 
+			}
+		}
+		if (n % 11 == 0)
+		{
+			invalid = 1;
+			for (int i = 0; i < n / 11; i++) {
+				if (!(str[i] == str[n / 11 + i]
+					&& str[i] == str[2 * n / 11 + i]
+					&& str[i] == str[3 * n / 11 + i]
+					&& str[i] == str[4 * n / 11 + i]
+					&& str[i] == str[5 * n / 11 + i]
+					&& str[i] == str[6 * n / 11 + i]
+					&& str[i] == str[7 * n / 11 + i]
+					&& str[i] == str[8 * n / 11 + i]
+					&& str[i] == str[9 * n / 11 + i]
+					&& str[i] == str[10 * n / 11 + i]
+				))
+					invalid = 0;
+			}
+			if (invalid) {
+				invalids += l;
+				std::cout << l << std::endl;
+				continue; 
+			}
+		}
+		if (n % 13 == 0)
+		{
+			invalid = 1;
+			for (int i = 0; i < n / 13; i++) {
+				if (!(str[i] == str[n / 13 + i]
+					&& str[i] == str[2 * n / 13 + i]
+					&& str[i] == str[3 * n / 13 + i]
+					&& str[i] == str[4 * n / 13 + i]
+					&& str[i] == str[5 * n / 13 + i]
+					&& str[i] == str[6 * n / 13 + i]
+					&& str[i] == str[7 * n / 13 + i]
+					&& str[i] == str[8 * n / 13 + i]
+					&& str[i] == str[9 * n / 13 + i]
+					&& str[i] == str[10 * n / 13 + i]
+					&& str[i] == str[11 * n / 13 + i]
+					&& str[i] == str[12 * n / 13 + i]
+				))
+					invalid = 0;
+			}
+			if (invalid) {
+				invalids += l;
+				std::cout << l << std::endl;
+				continue; 
+			}
+		}
+		if (n % 15 == 0)
+		{
+			invalid = 1;
+			for (int i = 0; i < n / 15; i++) {
+				if (!(str[i] == str[n / 15 + i]
+					&& str[i] == str[2 * n / 15 + i]
+					&& str[i] == str[3 * n / 15 + i]
+					&& str[i] == str[4 * n / 15 + i]
+					&& str[i] == str[5 * n / 15 + i]
+					&& str[i] == str[6 * n / 15 + i]
+					&& str[i] == str[7 * n / 15 + i]
+					&& str[i] == str[8 * n / 15 + i]
+					&& str[i] == str[9 * n / 15 + i]
+					&& str[i] == str[10 * n / 15 + i]
+					&& str[i] == str[11 * n / 15 + i]
+					&& str[i] == str[12 * n / 15 + i]
+					&& str[i] == str[13 * n / 15 + i]
+					&& str[i] == str[14 * n / 15 + i]
+				))
+					invalid = 0;
+			}
+			if (invalid) {
+				invalids += l;
+				std::cout << l << std::endl;
+				continue; 
+			}
+		}
+	}
+	return invalids;
 }
 
 void	part1()
 {
-	int	result = 0;
+	long long	result = 0;
 	std::ifstream file("input.txt");
 	// Check if the file is open
 	if (!file.is_open()) {
@@ -21,47 +311,32 @@ void	part1()
 	}
 
 	std::string		line;
-	int				a;
+	long long		a;
+	long long		b;
+	long long		invalids = 0;
 	int				dial = 50;
+	std::vector<long long> numbers;
 
 	// Read the file line by line
-	while (std::getline(file, line)) {
-		std::cout << line << std::endl;  // Print each line to the console
-		if (line[0] == 'R')
-			line[0] = '0';
-		else
-			line[0] = '-';
-		convertStringToIntegers(line, a);
-		dial += a;
-		while (dial < 0) {
-			dial += 100;
-		}
-		while (dial > 99) {
-			dial -= 100;
-		}
-		if (dial == 0)
-			result++;
-		std::cout << "first: " << a << std::endl;
+	std::getline(file, line);
+	convertStringToIntegers(line, numbers);
+	int n = numbers.size();
+	for (int i = 0; i < n; i += 2)
+	{
+		a = numbers[i];
+		b = numbers[i + 1];
+		invalids += find_invalids(a, b);
+		std::cout << a << " " << b << "   " << invalids << std::endl;
+
 	}
-	// std::multiset<int>::iterator it1 = first.begin();
-	// std::multiset<int>::iterator it2 = second.begin();
-	// int	dif;
-	// while (it1 != first.end()) {
-	// 	dif = *it1 - *it2;
-	// 	if (dif < 0)
-	// 		dif = -dif;
-	// 	result += dif;
-	// 	it1++;
-	// 	it2++;
-	// }
-	// Close the file
+	result = invalids;
 	file.close();
 	std::cout << result << std::endl;
 }
 
 void	part2()
 {
-	int	result = 0;
+	long long	result = 0;
 	std::ifstream file("input.txt");
 	// Check if the file is open
 	if (!file.is_open()) {
@@ -70,63 +345,25 @@ void	part2()
 	}
 
 	std::string		line;
-	int				a;
+	long long		a;
+	long long		b;
+	long long		invalids = 0;
 	int				dial = 50;
+	std::vector<long long> numbers;
 
 	// Read the file line by line
-	while (std::getline(file, line)) {
-		auto zero = false;
-		// std::cout << line << std::endl;  // Print each line to the console
-		if (line[0] == 'R')
-			line[0] = '0';
-		else
-			line[0] = '-';
-		convertStringToIntegers(line, a);
-		std::cout << "a" << dial << std::endl;
-		if (a > 0)
-		{
-			while (a) {
-				a--;
-				dial++;
-				if (dial == 100)
-				{
-					dial = 0;
-					result++;
-				}
-			}
-		}
-		else if (a < 0)
-		{
-			while (a) {
-				a++;
-				dial--;
-				if (dial == 0)
-				{
-					result++;
-				}
-				if (dial == -1)
-				{
-					dial = 99;
-				}
-			}
-		}
-		std::cout << "r" << result << std::endl;
-		// if (dial == 0)
-		// 	result++;
-		// std::cout << "first: " << a << std::endl;
+	std::getline(file, line);
+	convertStringToIntegers(line, numbers);
+	int n = numbers.size();
+	for (int i = 0; i < n; i += 2)
+	{
+		a = numbers[i];
+		b = numbers[i + 1];
+		invalids += find_invalids2(a, b);
+		std::cout << a << " " << b << "   " << invalids << std::endl;
+
 	}
-	// std::multiset<int>::iterator it1 = first.begin();
-	// std::multiset<int>::iterator it2 = second.begin();
-	// int	dif;
-	// while (it1 != first.end()) {
-	// 	dif = *it1 - *it2;
-	// 	if (dif < 0)
-	// 		dif = -dif;
-	// 	result += dif;
-	// 	it1++;
-	// 	it2++;
-	// }
-	// Close the file
+	result = invalids;
 	file.close();
 	std::cout << result << std::endl;
 }
